746a6714fada6922eae899691546cf161e55c225
Cireneu Guilherme <cireneu@fotonica.ifce.edu.br>
2018-05-14 15:49:02 -0300

    Commit para upar pro heroku8

--- resources/views/produto.blade.php
+++ resources/views/produto.blade.php
@@ -43,27 +43,28 @@
                                 <div class="media">
                                     <ul class="tab-avatars" data-anime="scroll">
                                         <li>
-                                            <figure class="figure">
+                                        <!--<figure class="figure">
                                                 @if(Request::is('editando/*'))
                                                     <img style="width: 200px; height: 200px" src="{{asset('img/fotos/'.$produto->foto)}}" class="figure-img img-fluid " >
                                                 @else
                                                     <img style="width: 200px; height: 200px" src="{{asset('img/fotos/default.png')}}" class="figure-img img-fluid " >
                                                 @endif
 
-                                                    <figcaption class="figure-caption">
+                                                 <figcaption class="figure-caption">
                                                     <input type="hidden" name="MAX_FILE_SIZE" value="99999999"/>
                                                     <div><input value="Escolher imagem" name="imagem" type="file"/></div>
                                                 </figcaption>
-                                            </figure>
+                                            </figure>-->
                                         </li>
                                     </ul>
                                     <ul class="">
                                         <li><label>* Categoria</label>
+                                           <!--
                                             <select name="categoria" id="">
-                                                @foreach($categorias as $categoria)
+                                                foreach($categorias as $categoria)
                                                     <option value="{{$categoria->id}}">{{$categoria->nome_categoria}}</option>
-                                                @endforeach
-                                            </select>
+                                                endforeach
+                                            </select>-->
                                         </li>
                                         <li>
                                             {!! Form::label('nome_produto','*Nome:') !!}
